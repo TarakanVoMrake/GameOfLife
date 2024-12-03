5247484f65514bda4e24357241207a66717e07a0
TarakanVoMrake <71223228+TarakanVoMrake@users.noreply.github.com>
2024-12-03 11:56:49 +0100

    End 1 step

--- js/script.js
+++ js/script.js
@@ -1,35 +1,37 @@
 let colorCells = "#222222";
 let destiny = 0.5;
 let sizeField = 10;
-let sizeCanvas = 750;
+let sizeCanvas = 600;
 let lengthCells = sizeCanvas / sizeField;
-//let field = [];
-//let newField = [];
 let aliveCells = new Set();
 let newAliveCells = new Set();
 let changedCells = new Set();
+let potentialСells = new Set();
 let living = false;
 const stepsInterval = [2000, 1000, 500, 250, 125, 60, 40, 20];
-let stepInterval = stepsInterval[3];
+let stepInterval= stepsInterval[3];
 
 
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext("2d");
+
+context.imageSmoothingEnabled = false;
 /*window.devicePixelRatio для нахождения соотношения (плотности) физического количества пикселей на устройстве к
 логическому количеству пикселей,  которое браузер использует для отображения содержимого.
 ||1 ИЛИ 1,  используется для того, чтобы задать значение по умолчанию*/
 const dpr = window.devicePixelRatio || 1;
+// const dpr = 1;
 console.log(dpr);
 // Задаем размер канваса с учетом плотности пикселей
 canvas.width = sizeCanvas * dpr;
-canvas.height = sizeCanvas * dpr;
+canvas.height =  sizeCanvas * dpr;
 // Задаем размеры, чтобы они оставались sizeCanvas x sizeCanvas
-canvas.style.width = String(sizeCanvas)+ "px"; 
-canvas.style.height = String(sizeCanvas) + "px";
+canvas.style.width = `${sizeCanvas}px`; 
+canvas.style.height = `${sizeCanvas}px`;
 // Масштабируем контекст
 context.scale(dpr, dpr);
-
+context.lineWidth = 1;
 
 generateRandomField();
 
@@ -38,20 +40,59 @@ generateRandomField();
 // Рисуем
 function drawField() {
     console.log("drawField");
-    for (cell of changedCells) {
-        //ERROR 
-        if (aliveCells.has(cell)) {
-            context.fillStyle = colorCells;
-        } else context.fillStyle = "#ffffff";
-        
-        // console.log(cell[0], cell[1]);
+    context.fillStyle = "#ffffff";
         context.fillRect(
-            cell[0] * lengthCells,
-            cell[1] * lengthCells,
+            0,
+            0,
+            sizeCanvas,
+            sizeCanvas
+        );
+    context.fillStyle = colorCells;
+    for (const cell of aliveCells) {
+        const cell_ = strToArr(cell);
+
+        context.fillRect(
+            cell_[0] * lengthCells,
+            cell_[1] * lengthCells,
             lengthCells,
             lengthCells
         );
     }
+    /*for (cell of changedCells) {
+        context.fillStyle = aliveCells.has(cell) ? colorCells : "#ffffff";
+        const cell_ = strToArr(cell);
+
+        context.fillRect(
+            cell_[0] * lengthCells,
+            cell_[1] * lengthCells,
+            lengthCells,
+            lengthCells
+        );
+
+        // context.fillRect(
+        //     Math.floor(cell_[0] * lengthCells),
+        //     Math.floor(cell_[1] * lengthCells),
+        //     Math.ceil(lengthCells),
+        //     Math.ceil(lengthCells)
+        // );
+        const cell_ = strToArr(cell);
+        context.fillStyle = colorCells;
+        if (aliveCells.has(cell)) {
+            context.fillRect(
+                cell_[0] * lengthCells,
+                cell_[1] * lengthCells,
+                lengthCells,
+                lengthCells
+            );
+        } else {
+            context.clearRect(
+                cell_[0] * lengthCells,
+                cell_[1] * lengthCells,
+                lengthCells,
+                lengthCells
+            );
+        }
+    }*/
     changedCells.clear();
 };
 
@@ -76,6 +117,20 @@ function generateRandomField () {
     drawField();    
 }
 
+//Make random field
+function random () {
+    console.log("Random");
+    for (let i = 0; i < sizeField; i++) {
+        for (let j = 0; j < sizeField; j++) {
+            if (Math.random() < destiny) {
+                aliveCells.add([i, j].join(","));
+                changedCells.add([i, j].join(","));
+            } else {
+            }
+        }
+    }
+}
+
 function clearField () {
     console.log("clearField");
     stop();
@@ -83,118 +138,73 @@ function clearField () {
     changedCells.clear();
     context.fillStyle = "#ffffff";
     context.fillRect(0, 0, sizeCanvas, sizeCanvas);
-    //drawField();
 }
 
 //step
 function step () {
     console.log("Step");
+    findPotentialСells();    
     newAliveCells.clear();
-    for (const cell of aliveCells) {
-        checkNeighbors(cell[0], cell[1]);
+    for (const cell of potentialСells) {
+        const cell_ = strToArr(cell);
+        checkNeighbors(cell_[0], cell_[1]);
     }
     aliveCells = new Set(newAliveCells);
     drawField();
 }
 
-//Make random field
-function random () {
-    console.log("Random");
-    for (let i = 0; i < sizeField; i++) {
-        for (let j = 0; j < sizeField; j++) {
-            if (Math.random() < destiny) {
-                console.log("add");
-                aliveCells.add([i, j]);
-                changedCells.add([i, j]);
-            } else {
-            }
-        }
+function findPotentialСells() {
+    potentialСells.clear();
+
+    for (cell of aliveCells) {
+        const cell_ = strToArr(cell);
+        const left = (cell_[0] - 1 + sizeField) % sizeField;
+        const right = (cell_[0] + 1 + sizeField) % sizeField;
+        const up = (cell_[1] - 1 + sizeField) % sizeField;
+        const down = (cell_[1] + 1 + sizeField) % sizeField;
+
+
+        potentialСells.add([left,       up].join(','));
+        potentialСells.add([left,       cell_[1]].join(','));
+        potentialСells.add([left,       down].join(','));
+        potentialСells.add([cell_[0],   up].join(','));
+        potentialСells.add([cell_[0],   cell_[1]].join(','));
+        potentialСells.add([cell_[0],   down].join(','));
+        potentialСells.add([right,      up].join(','));
+        potentialСells.add([right,      cell_[1]].join(','));
+        potentialСells.add([right,      down].join(','));
     }
 }
 
-// function checkNeighbors(i, j) {
-//         let countAliveNeighbors = 
-//             field[(i - 1 + sizeField) % sizeField][(j - 1 + sizeField) % sizeField] + 
-//             field[(i - 1 + sizeField) % sizeField][j] + 
-//             field[(i - 1 + sizeField) % sizeField][(j + 1 + sizeField) % sizeField] + 
-//             field[i][(j - 1 + sizeField) % sizeField] + 
-//             field[i][(j + 1 + sizeField) % sizeField] + 
-//             field[(i + 1 + sizeField) % sizeField][(j - 1 + sizeField) % sizeField] + 
-//             field[(i + 1 + sizeField) % sizeField][j] + 
-//             field[(i + 1 + sizeField) % sizeField][(j + 1 + sizeField) % sizeField];
-//     if (field[i][j] === 1) {
-//             console.log(" i = " + i + " j = " + j, "countAliveNeighbors = " , countAliveNeighbors);
-//             console.log("i = " + i);
-//             console.log("i - 1 = " + (i - 1));
-//             console.log("sF = " + sizeField);
-            
-//             console.log("i - 1 + sF = " + (i - 1 + sizeField) );
-            
-//             console.log((i - 1 + sizeField) );
-//             console.log((j - 1 + sizeField) );
-            
-//             console.log(field[(i - 1 + sizeField) % sizeField][(j - 1 + sizeField) % sizeField]);
-//             console.log(field[(i - 1 + sizeField) % sizeField][j]);
-//             console.log(field[(i - 1 + sizeField) % sizeField][(j + 1 + sizeField) % sizeField]);
-//             console.log(field[i][(j - 1 + sizeField) % sizeField]);
-//             console.log(field[i][(j + 1 + sizeField) % sizeField]);
-//             console.log(field[(i + 1 + sizeField) % sizeField][(j - 1 + sizeField) % sizeField]);
-//             console.log(field[(i + 1 + sizeField) % sizeField][j]);
-//             console.log(field[(i + 1 + sizeField) % sizeField][(j + 1 + sizeField) % sizeField]);
-            
-            
-            
-                        
-//         if (countAliveNeighbors === 2 || countAliveNeighbors === 3) {newField[i][j] = 1;
-//             console.log("+ => +");
-            
-//         } 
-//         else  {
-//             newField[i][j] = 0;
-//             console.log("+ => -");
-//         }
-//     } else {
-//             //console.log("-i = " + i + " -j = " + j, "countAliveNeighbors = " , countAliveNeighbors);
-            
-//         if (countAliveNeighbors === 3) {
-//             newField[i][j] = 1;
-//             console.log("- => +");
-//         }
-//         else  {
-//             newField[i][j] = 0;
-//             console.log("- => -");
-//         }
-//     }
-// }
 function checkNeighbors(i, j) {
-    const up = (i - 1 + sizeField) % sizeField;
-    const down = (i + 1 + sizeField) % sizeField;
-    const left = (j - 1 + sizeField) % sizeField;
-    const right = (j + 1 + sizeField) % sizeField;
-
+    const left = (i - 1 + sizeField) % sizeField;    
+    const right = (i + 1 + sizeField) % sizeField;
+    const up = (j - 1 + sizeField) % sizeField;
+    const down = (j + 1 + sizeField) % sizeField;
 
     let countAliveNeighbors = 0;
-    if (aliveCells.has([up, left])) countAliveNeighbors++;
-    if (aliveCells.has([up, i])) countAliveNeighbors++;
-    if (aliveCells.has([up, right])) countAliveNeighbors++;
-    if (aliveCells.has([j, left])) countAliveNeighbors++;
-    if (aliveCells.has([j, right])) countAliveNeighbors++;
-    if (aliveCells.has([down, left])) countAliveNeighbors++;
-    if (aliveCells.has([down, i])) countAliveNeighbors++;
-    if (aliveCells.has([down, right])) countAliveNeighbors++;
+    if (aliveCells.has([left,     up].join(','))) countAliveNeighbors++;
+    if (aliveCells.has([left,     j].join(','))) countAliveNeighbors++;
+    if (aliveCells.has([left,     down].join(','))) countAliveNeighbors++;
+    if (aliveCells.has([i,      up].join(','))) countAliveNeighbors++;
+    if (aliveCells.has([i,      down].join(','))) countAliveNeighbors++;
+    if (aliveCells.has([right,   up].join(','))) countAliveNeighbors++;
+    if (aliveCells.has([right,   j].join(','))) countAliveNeighbors++;
+    if (aliveCells.has([right,   down].join(','))) countAliveNeighbors++;
 
-    if (aliveCells.has([i, j])) {
+
+    const cell = [i, j].join(',');    
+    if (aliveCells.has(cell)) {
         if ((countAliveNeighbors === 2 || countAliveNeighbors === 3)) {
-            newAliveCells.add([i, j]);
+            newAliveCells.add(cell);
         } else {
-            changedCells.add([i, j]);
+            changedCells.add(cell);
         }
     } else if (countAliveNeighbors === 3) {
-        newAliveCells.add([i, j]);
-        changedCells.add([i, j]);
+        newAliveCells.add(cell);
+        changedCells.add(cell);
     }
 }
-
 
 const myCanvas = document.getElementById("canvas");
 myCanvas.addEventListener('click', (event) => {
@@ -203,10 +213,14 @@ myCanvas.addEventListener('click', (event) => {
     let top = rect.top;
     let x = event.clientX - left;
     let y = event.clientY - top;
-    let column = Math.floor(x / lengthCells);
     let row = Math.floor(y / lengthCells);
-    if (field[row][column] === 1) field[row][column] = 0;
-    else field[row][column] = 1;
+    let column = Math.floor(x / lengthCells);
+    const cell_ = [column, row].join(",");
+
+    if (!aliveCells.delete(cell_)) {
+        aliveCells.add(cell_);
+    }
+    changedCells.add(cell_);    
     drawField();
 });
 
@@ -219,10 +233,17 @@ function setBgColor (input) {
 
 function  setSize (input) {
     console.log("SetSize");
+     
+    const number = document.getElementById('size-field').lastElementChild;
+    const range = number.previousElementSibling;
     
     sizeField = Number(input.value);
+    if (sizeField < 5) sizeField = 5;
+    if (sizeField > 200) sizeField = 200;
+    number.value = sizeField;
+    range.value = sizeField;
     lengthCells = sizeCanvas / sizeField;
-    console.log(sizeField);
+    console.log("sizeField", sizeField);
     
     generateRandomField();
 }
@@ -235,34 +256,19 @@ function  setSpeed (input) {
         play();
     }
 }
-//TO DO
-/*const slider = document.getElementById('slider');
-const output = document.getElementById('output');
-      
-slider.addEventListener('input', () => {
-    output.value = slider.value;
-});
-
-output.addEventListener('input', () => {
-    if (output.value >= slider.min && output.value <= slider.max) {
-        slider.value = output.value;
-    }
-});*/
 
 function print () {
     console.log("======INFO====");
     console.log("sizeField = " + sizeField);
-    console.log("Field:");
-    console.log(...field);
-    console.log("newField:");
-    console.log(...newField);
+    console.log("aliveCells:");
+    console.log(...aliveCells);
+    console.log("newAliveCells:");
+    console.log(...newAliveCells);
+    console.log("potentialСells:");
+    console.log(...potentialСells);
     console.log("===========");
 }
 
-function print1 () {
-    for (let i = 0; i < sizeField; i++) {
-        for (let j = 0; j < sizeField; j++) {
-            if(field[i][j] === 1) console.log(" i = " + i + " j = " + j, "isLive = " + field[i][j])
-        }
-    }   
+function strToArr (cell) {
+    return cell.split(",").map(Number);
 }
